6761a9514d049f09914172fbaa0b51a908914bb8
Arpo <arpo555@hotmail.com>
2020-12-18 14:10:04 +0700

    update togle mode in navbar

---
+++ script.js
@@ -0,0 +1,16 @@
+let isclick = false;
+
+function toggle(){
+    var tog1 = document.getElementById("tog-1");
+    var tog2 = document.getElementById("tog-2");
+
+    if(!isclick){
+        tog1.classList.add("show1");
+        tog2.classList.add("show2");
+        isclick = true;
+    }else if(isclick){
+        tog1.classList.remove("show1");
+        tog2.classList.remove("show2");
+        isclick = false;
+    }
+}
