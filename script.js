7a56293f7170401c34d95061834fc5963eb3d3f4
Arpo <arpo555@hotmail.com>
2020-12-18 20:34:41 +0700

    update contact page

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function toggle(){
         isclick = false;
     }
 }
+
+function getData(){
+    var blob = new Blob(["Welcome to Websparrow.org."],
+                { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "static.txt");
+}
